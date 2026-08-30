a7153ccc13e2be288d995be9e01254f0a66e7194
Amaro <ryukyagami3@gmail.com>
2026-08-30 19:13:09 -0300

    menu

--- ProjetoEscola/main.c
+++ ProjetoEscola/main.c
@@ -1,1 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
 
+int main(void) {
+
+    int opcao;
+
+    int sair = 0 //falso
+
+    while(!sair) {
+        printf("====================\n");
+        printf("Projeto Escola\n");
+        printf("====================\n");
+        printf("0 - Sair\n");
+        printf("1 - Aluno\n");
+        printf("2 - Professor\n");
+        printf("3 - Disciplina\n");
+
+        scanf("%d", opcao);
+
+        switch(opcao) {
+            case 0: {
+                sair = 1;
+                break;
+            }
+            case 1: {
+                printf("1 - Aluno\n");
+                break;
+            }
+            case 2: {
+                printf("2 - Professor\n");
+                break;
+            }
+            case 3: {
+                printf("3 - Disciplina\n");
+                break;
+            } default: {
+                printf("Opção Inválida\n");
+                }
+            }
+        }
+
+        return 0
+    }
